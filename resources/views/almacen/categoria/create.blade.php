f73345271a9ba0238343ecefd1e26183f7f49231
Juan Federico Kaenel <Kaenel_Juan@hotmail.com>
2020-06-14 21:26:35 -0300

    Agregando vistas y plantillas blade, instalando laravel collective.

---
+++ resources/views/almacen/categoria/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('contenido')
+    {{--el contenido de lo q copie en la plantilla se va amostrar en la section de contenido renombrada como yield en admin --}}
+    <h3>Create</h3>
+
+@stop
