af8eea2fd5bf5b7160e56774314b83c6aeb201b2
Juan <kaenel10@gmail.com>
2020-06-21 19:02:54 -0300

    Controlador Ingreso

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\DetalleIngreso;
+use App\Http\Requests\IngresoFormRequest;
 use App\Http\Requests\PersonaFormRequest;
+use App\Ingreso;
 use App\Persona;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -41,55 +45,79 @@ class IngresoController extends Controller
         return view("compras.ingreso.create",["personas"=>"$personas","articulos"=>"$articulos"]);
     }
 
-    //SI EL METODO DE ENVIO DEL FORMULARIO ES POST LLAMA A LA FUNCION STORE
-    public function store(PersonaFormRequest $request)
+    //nos permitirá guardar tanto los ingresos como los detalles de ingreso
+    public function store(IngresoFormRequest $request)
     {
-        $persona = new Persona;
-        $persona->tipo_persona= 'Proveedor';
-        $persona->nombre = $request->get('nombre');
-        $persona->tipo_documento = $request->get('tipo_documento');
-        $persona->num_documento = $request->get('num_documento');
-        $persona->telefono = $request->get('telefono');
-        $persona->email = $request->get('email');
-        $persona->direccion = $request->get('direccion');
+        try {
+         DB::beginTransaction();//inicio la transaccion   ----   almacenamos primero en la base de datos primero el ingreso y dsp el detalle de ingreso, pero los dos deben almacenarse*/
+            //tabla ingreso
+            $ingreso = new Ingreso();
+            $ingreso->idproveedor=$request->get('idproveedor');
+            $ingreso->tipo_comprobante=$request->get('tipo_comprobante');
+            $ingreso->serie_comprobante=$request->get('serie_comprobante');
+            $ingreso->num_comprobante= $request->get('num_comprobante');
+            $mytime = Carbon::now('America/Argentina/Cordoba');
+            $ingreso->fecha_hora=$mytime->toDateString();
+            $ingreso->impuesto='18';
+            $ingreso->estado='A';
+            $ingreso->save();
 
-        $persona->save();
+            /*estos datos vienen del mismo formulario*/
 
-        return redirect('compras/proveedor');
+            //Tabla detalle_ingreso
+            $idarticulo= $request->get('idarticulo');
+            $cantidad = $request->get('cantidad');
+            $precio_compra = $request->get('precio_compra');
+            $precio_venta = $request->get('precio_venta');
+
+            //esto hago porque se va a enviar un array de detalles
+            $cont = 0; //esto me va a controlar los articulos
+            while ($cont < count($idarticulo)){
+                $cont = $cont+1;
+                $detalle = new DetalleIngreso();
+                $detalle->idingreso = $ingreso->idingreso;
+                $detalle->idarticulo = $ingreso->idarticulo[$cont]; //esto ya va a depender del articulo en la posicion tal del input q se envia
+                $detalle->cantidad = $ingreso->cantidad[$cont];
+                $detalle->precio_compra= $ingreso->precio_compra[$cont];
+                $detalle->precio_venta = $ingreso->precio_venta[$cont];
+                $detalle->save();
+
+                $cont= $cont+1;
+            }
+
+
+         DB:commit();//finalizo la transaccion
+        }catch (\Exception $e){
+            DB::rollback(); //si ocurre un error cancelo la transacción
+        }
+
+        return redirect('compras/ingreso');
     }
 
+    //Para mostrar los ingresos y los detalles en una vista
     public function show($id)
     {
-        return view('compras.proveedor.show',['persona'=>Persona::findOrFail($id)]);
+        $ingreso = DB::table('ingreso as i')
+            ->join('persona as p','i.proveedor','=','p.idpersona')
+            ->join('detalle_ingreso as di', 'i.idingreso','=','di.idingreso')
+            ->select('i.idingreso','i.fecha_hora','p.nombre','i.tipo_comprobante','i.serie_comprobante','i.num_comprobante','i.impuesto','i.estado',DB::raw('sum(di.cantidad*precio_compra) as total'))
+            ->where('i.ingreso','=',$id)
+            ->first(); //solo mostrame el primer ingreso q cumpla, porque solo va a cumplir un ingreso
+
+        $detalles = DB::table('detalle_ingreso as d')
+            ->join('articulo as a','d.idarticulo','=','a.idarticulo')
+            ->select('a.nombre as articulo','d.cantidad','d.precio','d.precio_compra','d.precio_venta')
+            ->where('d.idingreso','=',$id)->get(); //de ese detalle ingreso especifico, hacer el filtro de articulo y detalle ingreso y mostrame el nombre el articulo la cantidad el precio de compra y venta
+        return view('compras.ingreso.show',['ingreso'=>$ingreso,'detalles'=>$detalles]);/*le mando el array ingreso con todos los valores y tmb el detalle*/
     }
 
-    public function edit($id)
-    {
-        return view('compras.proveedor.edit',['persona'=>Persona::findOrFail($id)]);
-    }
-
-    //SI EL METODO DE ENVIO DEL FORMULARIO ES PATCH LLAMA A LA FUNCION UPDATE
-    public function update(PersonaFormRequest $request, $id)
-    {
-        $persona = Persona::findOrFail($id);
-        //el tipo de persona no porque si voy a editar va a seguir siendo un cliente
-        $persona->nombre = $request->get('nombre');
-        $persona->tipo_documento = $request->get('tipo_documento');
-        $persona->num_documento = $request->get('num_documento');
-        $persona->telefono = $request->get('telefono');
-        $persona->email = $request->get('email');
-        $persona->direccion = $request->get('direccion');
-        $persona->update();
-
-        return redirect('compras/proveedor')->with('mensaje', '¡Persona editada!');
-    }
 
     //SI EL METODO DE ENVIO DEL FORMULARIO ES DELETE LLAMA A LA FUNCION UPDATE
     public function destroy($id)
     {
-        $persona = Persona::findOrFail($id);
-        $persona->tipo_persona = 'Inactivo'; /*cuando borro que pase a cero la condicion*/
-        $persona->update();
-        return redirect('compras/proveedor')->with('mensaje', 'Persona Eliminada');
+        $ingreso = Ingreso::findOrFail($id);
+        $ingreso->estado = 'C'; /*cuando borro que pase a cero la condicion*/
+        $ingreso->update();
+        return redirect('compras/ingreso')->with('mensaje', 'Ingreso Eliminado');
     }
 }
